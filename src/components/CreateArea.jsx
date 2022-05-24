98ed38416dda807975271def75ded370cf6b67b2
Saksham Gupta <skshmgupt@gmail.com>
2022-05-25 00:17:23 +0530

    Update

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -3,8 +3,6 @@ import AddIcon from '@mui/icons-material/Add';
 import Fab from '@mui/material/Fab';
 import Zoom from '@mui/material/Zoom';
 
-
-
 function CreateArea(props) {
   const [note, setNote] = useState({
     title: "",
